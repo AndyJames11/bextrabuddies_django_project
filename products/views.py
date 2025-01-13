87c318c30965f6467d9c4f6accbc7ab518fd6a7b
AndyJames11 <andrewjamesh11@googlemail.com>
2025-01-13 09:54:42 +0000

    category filters

--- products/views.py
+++ products/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-    
+
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria.")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -23,6 +29,8 @@ def all_products(request):
 
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
@@ -30,11 +38,11 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
-    
-    products = get_object_or_404(Product, pk=product_id)
+
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
