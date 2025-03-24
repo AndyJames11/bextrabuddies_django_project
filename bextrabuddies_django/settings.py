0ea6ef79d330a2a15c4b551e1df80ce41e5382af
AndyJames11 <andrewjamesh11@googlemail.com>
2025-03-24 14:45:58 +0000

    finish setting up product admin page

--- bextrabuddies_django/settings.py
+++ bextrabuddies_django/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    #'allauth.socialaccount',
+    'allauth.socialaccount',
     'home',
     'products',
     'bag',
