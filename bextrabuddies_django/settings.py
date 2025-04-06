50dfa944d9cb492e392b287a8cae09847ee4abdd
AndyJames11 <andrewjamesh11@googlemail.com>
2025-04-06 16:39:56 +0100

    change to postgresql instead of sqlite due to broken migrations

--- bextrabuddies_django/settings.py
+++ bextrabuddies_django/settings.py
@@ -134,12 +134,19 @@ WSGI_APPLICATION = 'bextrabuddies_django.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+#  }
+
+import dj_database_url
+
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
